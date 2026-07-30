0c8bcf1ae798d798d66251b08bc22c56cb41d1e5
Abolfazl Firoozi <firooziabolfazl2004@gmail.com>
2026-07-30 21:03:20 +0330

    feat: create contact data structure and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
+import { useState } from "react";
+import Contacts from "./components/Contacts";
 import Header from "./components/Header";
 
 function App() {
+  const [categories, setCategories] = useState(["Family", "Friend", "Colleague"]);
+  const [contacts, setContacts] = useState([
+    {
+      id: 1,
+      firstName: "Ali",
+      lastName: "Rezaei",
+      email: "ali.rezaei@example.com",
+      phone: "09121234567",
+      category: "Family",
+    },
+    {
+      id: 2,
+      firstName: "Sara",
+      lastName: "Mohammadi",
+      email: "sara.mohammadi@example.com",
+      phone: "09351234567",
+      category: "Friend",
+    },
+    {
+      id: 3,
+      firstName: "Amir",
+      lastName: "Karimi",
+      email: "amir.karimi@example.com",
+      phone: "09194567890",
+      category: "Colleague",
+    },
+    {
+      id: 4,
+      firstName: "Negar",
+      lastName: "Ahmadi",
+      email: "negar.ahmadi@example.com",
+      phone: "09901234567",
+      category: "Family",
+    },
+  ]);
 
   return (
     <> 
       <Header />
+      <Contacts />
     </>
   );
 }
---
+++ src/components/Contacts.jsx
@@ -0,0 +1,15 @@
+import styles from "./Contacts.module.css";
+
+function Contacts() {
+  return (
+    <>
+      <main className={styles.main}>
+        <h1>Contacts</h1>
+
+        <div></div>
+      </main>
+    </>
+  );
+}
+
+export default Contacts;
