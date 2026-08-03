139bc4ba2184bfa66b4deffeeade7cb4f4112ed8
Abolfazl Firoozi <firooziabolfazl2004@gmail.com>
2026-08-04 00:35:32 +0330

    fix: highlight active category filter buttons

--- src/components/CategoryButtons.jsx
+++ src/components/CategoryButtons.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import styles from "./CategoryButtons.module.css";
 
 function CategoryButtons({ categories, setFilterCategory }) {
+  const [activeCategory, setActiveCategory] = useState("All");
   return (
     <>
       <div className={styles.filterButtons}>
         <button
-          onClick={() => setFilterCategory("All")}
-          className={`${styles.button} ${styles.activeFilter}`}
+          onClick={() => {
+            setFilterCategory("All");
+            setActiveCategory("All");
+          }}
+          className={`${styles.button} ${activeCategory === "All" && styles.activeFilter}`}
         >
           All
         </button>
@@ -14,9 +19,12 @@ function CategoryButtons({ categories, setFilterCategory }) {
           return (
             <button
               key={category.id}
-              className={styles.button}
+              className={`${styles.button} ${activeCategory === category.name && styles.activeFilter}`}
               value={category.name}
-              onClick={() => setFilterCategory(category.name)}
+              onClick={() => {
+                setFilterCategory(category.name);
+                setActiveCategory(category.name);
+              }}
             >
               {category.name}
             </button>
